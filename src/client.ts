55eb1a87cba8d7182858b24a59e28146534ffa42
PW <pw@PWs-MacBook-Pro.local>
2026-03-01 12:15:40 +0530

    call resource from client

--- src/client.ts
+++ src/client.ts
@@ -52,6 +52,29 @@ async function main() {
                 } else {
                     await handleTool(tool);
                 }
+            case "Resources":
+                const resourceUri = await select({
+                    message: "Select a resource",
+                    choices: [
+                        ...(resources?.map((res: any) => ({
+                            name: res.annotations?.title || res.name,
+                            value: res.uri,
+                            description: res.description,
+                        })) || []),
+                        ...(resourceTemplates?.map((res: any) => ({
+                            name: res.annotations?.title || res.name,
+                            value: res.uri,
+                            description: res.description,
+                        })) || [])
+                    ]
+                })
+                const uri = resources.find((t: any) => t.uri === resourceUri)?.uri || resourceTemplates.find((t: any) => t.uri === resourceUri)?.uriTemplate
+                if (uri == null) {
+                    console.log("Resource not found")
+                    continue
+                } else {
+                    await handleResource(uri);
+                }
         }
     }
 
@@ -70,6 +93,28 @@ async function handleTool(tool: Tool) {
         arguments: args,
     })
     console.log("Tool result:", result)
+}
+
+async function handleResource(uri: string) {
+    let finalUri = uri
+
+    const paramMatches = uri.match(/{([^}]+)}/g)
+
+    if (paramMatches != null) {
+        for (const paramMatch of paramMatches) {
+            const paramName = paramMatch.replace("{", "").replace("}", "")
+            const paramValue = await input({
+                message: `Enter value for ${paramName}:`,
+            })
+            finalUri = finalUri.replace(paramMatch, paramValue)
+        }
+    }
+
+    const res = await mcp.readResource({
+        uri: finalUri,
+    })
+
+    console.log("Resource content:", res)
 }
 
 main().catch((error) => {
