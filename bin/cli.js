57476291c72886c90930f52cf91410048cdfd496
Satyam <satyam1990jha@gmail.com>
2026-03-02 18:04:18 +0530

    bug fix

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import("../build/server.js").then(({main}) => main()).catch((error) => {
     console.error("MCP Server Failed to Start : Error > ", error);
     process.exit(1);
